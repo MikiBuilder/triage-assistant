74f319ee834e03c8f518e8933f81bb371041418d
MikiBuilder <msebcn@gmail.com>
2026-07-21 22:44:33 +0200

    fix: skip composer scripts during build, run cache:clear at runtime

--- docker/render/start.sh
+++ docker/render/start.sh
@@ -2,4 +2,5 @@
 set -e
 export PORT=${PORT:-10000}
 envsubst '${PORT}' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
+php bin/console cache:clear --no-warmup || true
 exec supervisord -c /etc/supervisor/conf.d/supervisord.conf
